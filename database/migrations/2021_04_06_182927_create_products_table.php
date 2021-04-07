21f74ee8fbd3cee8a8fa96b47df83a1543d3ced6
TrungDungNguyen <63592403+trdung1999@users.noreply.github.com>
2021-04-07 12:23:35 -0600

    add database products and posts

--- database/migrations/2021_04_06_182927_create_products_table.php
+++ database/migrations/2021_04_06_182927_create_products_table.php
@@ -15,7 +15,19 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('short_description')->nullable();
+            $table->string('description')->nullable();
+            $table->string('image');
+            $table->decimal('price');
+            $table->string('SKU');
+            $table->enum('stock_status',['Còn hàng','Hết hàng']);
+            $table->unsignedInteger('quantity')->default(10);
+            $table->enum('status',[0,1]);
+            $table->bigInteger('category_id')->unsigned()->nullable();
             $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
---
+++ database/migrations/2021_04_07_181907_create_news_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image')->nullable();
+            $table->text('content');
+            $table->integer('view_count');
+            $table->bigInteger('post_category_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->foreign('post_category_id')->references('id')->on('post_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
